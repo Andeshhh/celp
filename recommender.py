0e311e172c1303a9bf3943a78a3e1704f40d7de5
emmatas <emmatas@github.com>
2019-05-13 14:13:28 +0200

    poging 20000328929

--- recommender.py
+++ recommender.py
@@ -3,15 +3,13 @@ from data import CITIES, BUSINESSES, USERS, REVIEWS, TIPS, CHECKINS
 import random
 
 def reviewed_businesses(user_id):
-"""
-Returns a list of the businesses the current user left a review on 
-"""
-user_reviews =[]
-for city, reviews in REVIEWS.items():
-    for review in reviews:
-        if review['user_id'] == user_id:
-            user_reviews.append(review['business_id'])
-        return(user_reviews)
+    # Returns a list of the businesses the current user left a review on 
+    user_reviews =[]
+    for city, reviews in REVIEWS.items():
+        for review in reviews:
+            if review['user_id'] == user_id:
+                user_reviews.append(review['business_id'])
+                return(user_reviews)
 
 
 def recommend(user_id=None, business_id=None, city=None, n=10):
@@ -29,15 +27,15 @@ def recommend(user_id=None, business_id=None, city=None, n=10):
         }
     """
 # if the user is not logged in, give random options
-if user_id == None:
+    if user_id == None:
+        if not city:
+            city = random.choice(CITIES)
+            return random.sample(BUSINESSES[city], n)
+
+    # if the user is logged in
+    reviewed = reviewed_businesses(user_id)
+    print(reviewed)
+
     if not city:
         city = random.choice(CITIES)
         return random.sample(BUSINESSES[city], n)
-
-# if the user is logged in
-reviewed = reviewed_businesses(user_id)
-print(reviewed)
-
-if not city:
-    city = random.choice(CITIES)
-return random.sample(BUSINESSES[city], n)
