1ff9abf1f9b8d1f7ef03ff1dca40ee34a020dfbf
Andeshhh <46585918+Andeshhh@users.noreply.github.com>
2019-05-13 14:03:43 +0200

    Emma's update

--- recommender.py
+++ recommender.py
@@ -2,7 +2,20 @@ from data import CITIES, BUSINESSES, USERS, REVIEWS, TIPS, CHECKINS
 
 import random
 
+def reviewed_businesses(user_id):
+"""
+Returns a list of the businesses the current user left a review on 
+"""
+user_reviews =[]
+for city, reviews in REVIEWS.items():
+    for review in reviews:
+        if review['user_id'] == user_id:
+            user_reviews.append(review['business_id'])
+        return(user_reviews)
+
+
 def recommend(user_id=None, business_id=None, city=None, n=10):
+    
     """
     Returns n recommendations as a list of dicts.
     Optionally takes in a user_id, business_id and/or city.
@@ -15,6 +28,16 @@ def recommend(user_id=None, business_id=None, city=None, n=10):
             adress:str
         }
     """
+# if the user is not logged in, give random options
+if user_id == None:
     if not city:
         city = random.choice(CITIES)
-    return random.sample(BUSINESSES[city], n)
+        return random.sample(BUSINESSES[city], n)
+
+# if the user is logged in
+reviewed = reviewed_businesses(user_id)
+print(reviewed)
+
+if not city:
+    city = random.choice(CITIES)
+return random.sample(BUSINESSES[city], n)
